8cbe0a99196502efdfb50d3eac0ec9cf17f15602
Sergio Vidal <s.vidal.bravo.92@gmail.com>
2024-10-16 21:26:06 -0300

    Second commit

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,10 @@ import Tarjeta from './components/Tarjeta.vue';
 import Producto from './components/Producto.vue';
 import Avatar from './components/Avatar.vue';
 import Usuario from './components/Usuario.vue';
-
-
+import EstadoPedido from './components/EstadoPedido.vue';
 </script>
+
 <script>
-
-
 export default {
   components: {
     Usuario
@@ -30,15 +28,16 @@ export default {
 <template>
 <Tarjeta nombre="Chaqueta" descripcion="Esto es una chaqueta." />
 <Producto nombre="Laptop" :precio="100000" />
+<Producto nombre="camara" :precio="120000" />
 <Avatar url="https://via.placeholder.com/150" :tamaño="100" />
 <div class="d-flex flex-wrap">
     <Usuario :usuario="usuario1" />
-
-  </div>
-
+</div>
+<EstadoPedido estado="enviado" />
+<EstadoPedido estado="pendiente" />
+<EstadoPedido estado="cancelado" />
 </template>
 
 
 <style scoped>
-
 </style>
---
+++ src/components/EstadoPedido.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="alert" :class="estadoClass" role="alert">
+      {{ mensajeEstado }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "EstadoPedido",
+    props: {
+      estado: String,
+    },
+    computed: {
+      mensajeEstado() {
+        switch (this.estado) {
+          case "enviado":
+            return "Tu pedido ha sido enviado.";
+          case "pendiente":
+            return "Tu pedido está pendiente.";
+          case "cancelado":
+            return "Tu pedido ha sido cancelado.";
+          default:
+            return "Estado desconocido.";
+        }
+      },
+      estadoClass() {
+        switch (this.estado) {
+          case "enviado":
+            return "alert-success";
+          case "pendiente":
+            return "alert-warning";
+          case "cancelado":
+            return "alert-danger";
+          default:
+            return "alert-secondary";
+        }
+      },
+    },
+  };
+  </script>
